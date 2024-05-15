d5b39c072e237cecd3bffb1c037d0cce220204c5
Ali El-esawy <73000699+alielesawy@users.noreply.github.com>
2024-05-15 17:46:51 +0000

    Trying Deploy

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const seo = {
   og: {
     title: "Ali Esmaeil Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "https://alielesawy/github.io/alielesawy.github.io",
   },
 };
 
